7810ea7c40ea8d8339e1c1447a3bc662b3924609
edstv <100756977+edstv@users.noreply.github.com>
2023-07-11 12:56:57 +0530

    Update synapsesqlcicd_poc.sql

--- Security/synapsesqlcicd_poc.sql
+++ Security/synapsesqlcicd_poc.sql
@@ -1,5 +1,9 @@
 /****** Object:  Table [stage].[country_response_ext]    Script Date: 11-07-2023 11:42:51 ******/
+CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] WITH (FORMAT_TYPE = DELIMITEDTEXT, FORMAT_OPTIONS (FIELD_TERMINATOR = N',', FIRST_ROW = 2, USE_TYPE_DEFAULT = False))
+GO
 
+CREATE EXTERNAL DATA SOURCE [raw_syn01datalakestorage_dfs_core_windows_net] WITH (LOCATION = N'abfss://raw@syn02datalakestorage.dfs.core.windows.net')
+GO
 
 CREATE EXTERNAL TABLE [stage].[country_response_ext]
 (
@@ -27,10 +31,6 @@ WITH (DATA_SOURCE = [raw_syn01datalakestorage_dfs_core_windows_net],LOCATION = N
 GO
 
 
-CREATE EXTERNAL DATA SOURCE [raw_syn01datalakestorage_dfs_core_windows_net] WITH (LOCATION = N'abfss://raw@syn01datalakestorage.dfs.core.windows.net')
-GO
 
 
-CREATE EXTERNAL FILE FORMAT [SynapseDelimitedTextFormat] WITH (FORMAT_TYPE = DELIMITEDTEXT, FORMAT_OPTIONS (FIELD_TERMINATOR = N',', FIRST_ROW = 2, USE_TYPE_DEFAULT = False))
-GO
 
