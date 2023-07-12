a3de8ff246ded2b69275d6f9cb5084e265c185f0
edstv <100756977+edstv@users.noreply.github.com>
2023-07-12 15:47:35 +0530

    Update synapsesqlcicd_poc.sql

--- Security/synapsesqlcicd_poc.sql
+++ Security/synapsesqlcicd_poc.sql
@@ -49,7 +49,7 @@ SELECT [CompanyId]
     ,[IncotermId]
     ,getdate() AS datenow
     ,''abc'' AS test_string
-FROM OPENROWSET(BULK ''/aa/bb/A4LERF_Incoterms.parquet'', FORMAT = ''PARQUET'', DATA_SOURCE = ''rawpq_syn02datalakestorage_dfs_core_windows_net'') AS result')
+FROM OPENROWSET(BULK ''https://syn02datalakestorage.dfs.core.windows.net/rawpq/aa/bb/A4LERF_Incoterms.parquet'', FORMAT = ''PARQUET'') AS result')
 GO
 
 
